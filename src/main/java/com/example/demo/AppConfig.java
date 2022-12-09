15f96bbe3f42f5680dc93285caf6b0d3759f40ae
wogus4048 <wogus4048@naver.com>
2022-12-09 15:36:58 +0900

    8)AppConfig 리팩토링(구현이 잘안보이고, 중복이 있고 수정에 불편함이 있던부분)

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.discount.RateDiscountPolicy;
+import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemberService;
 import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.example.demo.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    private MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
+    }
+
+    private DiscountPolicy DiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
